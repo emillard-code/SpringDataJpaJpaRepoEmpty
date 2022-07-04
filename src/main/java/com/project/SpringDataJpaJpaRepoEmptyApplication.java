77c7c110eaaaa2d281a568ed3cb95f23a315d0ef
Emerson Millard <emillard.code@gmail.com>
2022-07-03 19:52:38 -0700

    Initial commit

---
+++ src/main/java/com/project/SpringDataJpaJpaRepoEmptyApplication.java
@@ -0,0 +1,15 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaJpaRepoEmptyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringDataJpaJpaRepoEmptyApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/project/springdatajpajparepoempty/SpringDataJpaJpaRepoEmptyApplicationTests.java
@@ -0,0 +1,8 @@
+package com.project.springdatajpajparepoempty;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaJpaRepoEmptyApplicationTests {
+
+}
